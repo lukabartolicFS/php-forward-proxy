f68b684bad81a5097228664f797f899dc51c5500
Luka Bartolic <lukabartolic@farshore.com>
2019-08-26 15:50:44 +0200

    added dotenv package and configuration

--- src/config.php
+++ src/config.php
@@ -1,8 +1,13 @@
 <?php
-    return [
-        'origin_endpoints' => [
-            'http://localhost/acacia_zoom_proxy/proxy_origin_test.php',
-            'http://localhost/acacia_zoom_proxy/proxy_origin_test2.php'
-        ]
-    ]
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
+$dotenv->load();
+
+return [
+    'origin_endpoints' => [
+        'http://localhost/acacia_zoom_proxy/proxy_origin_test.php',
+        'http://localhost/acacia_zoom_proxy/proxy_origin_test2.php'
+    ],
+    'zoom_verification_token' => getenv('ZOOM_VERIFICATION_TOKEN')
+]
 ?>
--- src/proxy.php
+++ src/proxy.php
@@ -6,23 +6,28 @@ use Proxy\Proxy;
 
 $config = include('config.php');
 $origins = $config['origin_endpoints'];
+$headers = apache_request_headers();
 
 $request = Request::createFromGlobals();
 $proxy = new Proxy();
 
-$proxy->getEventDispatcher()->addListener('request.sent', function($event) {
-	if($event['response']->getStatusCode() != 200){
-		die("Error");
-	}
-});
-$proxy->getEventDispatcher()->addListener('request.complete', function($event) use($request) {
-    $fp = file_put_contents('proxy.log', $request, FILE_APPEND);
-});
+if ($headers['Authorization'] == $config['zoom_verification_token']) {
+    $proxy->getEventDispatcher()->addListener('request.sent', function($event) {
+        if($event['response']->getStatusCode() != 200){
+            die("Error");
+        }
+    });
+    $proxy->getEventDispatcher()->addListener('request.complete', function($event) use($request) {
+        $fp = file_put_contents('proxy.log', date('Y-m-d H:i:s') . '\n' . $request, FILE_APPEND);
+    });
 
-foreach($origins as $origin) {
-    $response = $proxy->forward($request, $origin);
-    $response->send();
-    $fp = file_put_contents('proxy.log', "Forwarded to: {$origin}\n", FILE_APPEND);
+    foreach($origins as $origin) {
+        $response = $proxy->forward($request, $origin);
+        $response->send();
+    }
+}
+else {
+    $fp = file_put_contents('proxy.log', date('Y-m-d H:i:s') . ' ' . 'Authorization failed' . '[' . $headers['Authorization'] . ']' . ' ' . $request, FILE_APPEND);
 }
 
 ?>
